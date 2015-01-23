5b664bef750ae73fbb5364917fdfeeceeda98208
ecorkevin <kevinecor@gmail.com>
2015-01-23 15:25:31 -0600

    fix issue with unknown NODE_ENV

--- config/index.js
+++ config/index.js
@@ -1,2 +1,27 @@
-'use strict'
-module.exports = require('./'+(process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : "development"));
+module.exports = {
+  development: {
+    "port": 1337,
+    "redis": {
+      "host": "localhost",
+      "port": 6379
+    },
+    "errorPages": {
+      "404": "errors/404",
+      "not-connected": "errors/not-connected"
+    },
+    "development": true
+  },
+
+  production: {
+    "port": 1337,
+    "redis": {
+      "host": "localhost",
+      "port": 6379
+    },
+    "errorPages": {
+      "404": "errors/404",
+      "not-connected": "errors/not-connected"
+    },
+    "production": true
+  }
+};
